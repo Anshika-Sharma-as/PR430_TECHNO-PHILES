fd018e549c0b58a787088b6c48144fd5033fd0fc
ishita2301 <60536335+ishita2301@users.noreply.github.com>
2020-08-01 17:39:36 +0530

    made changes in mapping

--- urls.py
+++ urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path , include
+from uploadcontent import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('uploadcontent.urls'))
+    path('', views.index, name='index'),
+    path('upload', views.uploadImage , name = 'uploadImage'),
 ]
