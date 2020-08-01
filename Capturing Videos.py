566704155e2e90a944b8e70cfda202ae53a24f81
rishabhjain1799 <54940406+rishabhjain1799@users.noreply.github.com>
2020-08-01 12:54:19 +0530

    cODE MODIFIED

--- Capturing Videos.py
+++ Capturing Videos.py
@@ -13,6 +13,7 @@ cap = cv2.VideoCapture(0)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
 
+# Video Capturing started
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret==True:
