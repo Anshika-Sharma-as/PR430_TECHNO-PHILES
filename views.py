7970fc3381e54d77d031a383c2678dfa28a60a0d
ishita2301 <60536335+ishita2301@users.noreply.github.com>
2020-08-02 19:20:42 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,17 +1,20 @@
+import requests
 from django.shortcuts import render
-
-# Create your views here.
-
-def index(request):
-    return render( request, 'index.html')
-
-
-def uploadImage(request):
-    print("Request Handling.........")
-    p = request.FILES['image']
-    from . models import User
-    user = User(pictures = p)
-    user.save()
-    return render( request, 'index.html')
-
-    
+import sys
+from subprocess import run,PIPE
+from django.core.files.storage import FileSystemStorage
+def button(request):
+    return render(request,'home.html')
+def external(request):
+    video=request.FILES['video']
+    print("video is ",video)
+    fs=FileSystemStorage()
+    filename=fs.save(video.name,video)
+    fileurl=fs.open(filename)
+    templateurl=fs.url(filename)
+    print("file raw url",filename)
+    print("file full url", fileurl)
+    print("template url",templateurl)
+    video= run([sys.executable,'C://Users//ishit//Downloads//sih2020_model.py',str(fileurl),str(filename)],shell=False,stdout=PIPE,encoding='utf-8')
+    print(video.stdout)
+    return render(request,'home.html',{'raw_url':templateurl,'edit_url':video.stdout})
